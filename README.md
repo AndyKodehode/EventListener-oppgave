7ca8f8ace0f9530ba214eb69e58361fef0d9c9ed
AndyKodehode <git@stackblitz.com>
2024-10-29 18:00:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EventListener-oppgave
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/AndyKodehode/EventListener-oppgave)
---
+++ script.js
@@ -0,0 +1,17 @@
+let personName = document.querySelector('#name');
+let btn = document.querySelector('#btn');
+let showName = document.querySelector('#showName');
+
+//Make a variable that is an empty array
+
+// Make an EventListener for btn that listens for a click
+
+//INSIDE EVENTLISTENER//
+
+// When you click the button you should store the value of personName in a variable
+
+// update the text of showName to have the value of personName
+
+// Push the values of names into the array that you made so that you store all the names
+
+// Console.log the array and see if it updates when you write new names
